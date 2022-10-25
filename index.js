9317af0abfd8ddb67da20d348913571642836185
tanvirIIUC <tanvirsohaniiuc@gmail.com>
2022-10-25 17:16:23 +0600

    data update

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const port = 5000;
 
+const topics= require('./data/topics.json');
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+  })
+app.get('/topics', (req, res) => {
+    res.send(topics);
   })
 
   app.listen(port, () => {
