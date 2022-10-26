8a844254f667a702f12caadcdc679781243ba944
tanvirIIUC <tanvirsohaniiuc@gmail.com>
2022-10-26 20:26:53 +0600

    update

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const cors = require('cors');
 app.use(cors());
 
 const topics= require('./data/topics.json');
+const topicsDetails = require('./data/topicDetails.json');
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -13,6 +15,23 @@ app.get('/', (req, res) => {
 app.get('/topics', (req, res) => {
     res.send(topics);
   })
+
+app.get('/course/:id', (req, res) => {
+
+    const  id = req.params.id;
+    const all_course = topicsDetails.filter(n => n.category_id === id);
+    res.send(all_course );
+  })
+
+
+/* app.get('/news', (req, res) => {
+    res.send(topicsDetails);
+   
+  }) */
+/* app.get('/topicView/:id', (req, res) => {
+     const id = req.params.id;
+    console.log(req.params.id);
+  }) */
 
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
